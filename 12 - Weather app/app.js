d326aac4f4addd3c984f23ac2ba69cfcddb0a82d
jihading0 <71657888+jihading0@users.noreply.github.com>
2026-06-01 09:59:38 +0300

    12

---
+++ 12 - Weather app/app.js
@@ -0,0 +1,14 @@
+const apiKey = "93bbca04b543485bb78152614263105";
+
+const weather = document.querySelector(".Weather"); // form
+
+const input = document.querySelector(".Weather .inputSearch");
+const btn = document.querySelector(".Weather .btnSearch");
+
+weather.addEventListener("submit" , (e) => {
+
+    e.defaultPrevented();
+
+    console.log()
+
+})
