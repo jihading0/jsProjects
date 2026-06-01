31a9e3614fb6f7d8e982ef978c87f123bc0df076
jihading0 <71657888+jihading0@users.noreply.github.com>
2026-06-01 17:36:49 +0300

    project 12

--- 12 - Weather app/app.js
+++ 12 - Weather app/app.js
@@ -3,12 +3,47 @@ const apiKey = "93bbca04b543485bb78152614263105";
 const weather = document.querySelector(".Weather"); // form
 
 const input = document.querySelector(".Weather .inputSearch");
-const btn = document.querySelector(".Weather .btnSearch");
+
+const tempr = document.querySelector(".tempr");
+const descr = document.querySelector(".descr");
+const Feel = document.querySelector(".Feel");
+const Humidity = document.querySelector(".Humidity");
+const Speed = document.querySelector(".Speed");
 
 weather.addEventListener("submit" , (e) => {
 
-    e.defaultPrevented();
+    e.preventDefault();
+    const city = input.value.trim();
 
-    console.log()
 
+    console.log(city)
+    if(city) {
+        getWeater(city);
+    }
 })
+
+
+async function getWeater(city) {
+    try {
+         
+        const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=en`;
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error("المدينة غير موجودة أو حدث خطأ في الطلب");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        tempr.textContent = `${data.current.temp_c}°C`;
+        descr.textContent = `${data.current.condition.text}`;
+        Feel.textContent = `Feel Like : ${data.current.feelslike_f} °C`;
+        Humidity.textContent = `Humidity : ${data.current.humidity}%`;
+        Speed.textContent = `Wind Speed : ${data.current.wind_kph} m/s`;
+
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}   
